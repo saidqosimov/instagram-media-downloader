91f67770800508f8c5f0ab3a88c5d43f2d3bfbc7
saidqosimov <saidqosimov.social@gmail.com>
2024-08-29 16:44:47 +0500

    block and unblock channel

--- src/main/java/com/saidqosimov/instagrammediadownloader/entity/TelegramUsers.java
+++ src/main/java/com/saidqosimov/instagrammediadownloader/entity/TelegramUsers.java
@@ -28,3 +28,16 @@ public class TelegramUsers implements Serializable {
     private String username;
     private boolean enabled = true;
 }
+/*SELECT setval('telegram_users_id_seq', (SELECT MAX(id) FROM telegram_users));
+
+PGPASSWORD=kHHMEOcqeelVDLzGZGEYKaqnycgyBACa psql -h monorail.proxy.rlwy.net -U postgres -p 39850 -d railway
+
+CREATE TABLE telegram_users (
+    id SERIAL PRIMARY KEY,
+    chat_id BIGINT UNIQUE NOT NULL,
+    first_name VARCHAR(255),
+    lang VARCHAR(50),
+    username VARCHAR(32),
+    enabled BOOLEAN DEFAULT TRUE
+);
+ */
